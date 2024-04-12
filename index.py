e794e17314e4b356b75450a5ef3ee72d510dc11f
Lars Vinter <larsvinter@gmail.com>
2024-04-12 20:41:25 +0200

    updated response

--- index.py
+++ index.py
@@ -4,5 +4,9 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         "statusCode": 200,
-        "body": json.dumps("Hello from Lars!")
+        "body": json.dumps([
+            {
+                "bitrate": 128
+            }
+        ])
     }
