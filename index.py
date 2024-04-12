d77229019e2384e491caac586878fc6f9cebb492
Lars Vinter <larsvinter@gmail.com>
2024-04-12 20:44:28 +0200

    updated response

--- index.py
+++ index.py
@@ -2,11 +2,15 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    return {
-        "statusCode": 200,
-        "body": json.dumps([
-            {
-                "bitrate": 128
-            }
-        ])
-    }
+    return [
+        {
+            "streamId": 101,
+            "genre": "Top 40",
+            "bitrate": 128
+        },
+        {
+            "streamId": 102,
+            "genre": "Top 40",
+            "bitrate": 192
+        }
+    ]
